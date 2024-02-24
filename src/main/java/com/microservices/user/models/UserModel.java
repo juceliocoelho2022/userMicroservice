7618c5dd07af23ed314f2bbfafa502a1f4a3d861
Jucelio coelho <juceliocoelho2022@gmail.com>
2024-02-24 07:20:13 -0300

    Camada UserMicroservice concluida

---
+++ src/main/java/com/microservices/user/models/UserModel.java
@@ -0,0 +1,27 @@
+package com.microservices.user.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+@Getter
+@Setter
+public class UserModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+
+
+}
